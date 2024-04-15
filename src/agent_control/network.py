852521dd0f92d8e3a19e5371f0f0bdfabc9a9a3d
Anton_arl <CelestialArlate@gmail.com>
2024-04-15 14:25:55 +0700

    предварительная фиксация перед обучением

--- src/agent_control/network.py
+++ src/agent_control/network.py
@@ -1,18 +1,8 @@
 # https://www.youtube.com/watch?v=xMz7XSaqdRA&t=117s
 # Реализация нейросети при помощи numpy
 
+import time
 import numpy as np
-
-INPUT_DIM = 4 # количество входных значений
-OUT_DIM = 3 # количество вsходных значений
-H_DIM = 5 # количество нейронов на слое (пока 1 слой)
-
-x = np.random.randn(INPUT_DIM) # Входящий вектор, пока задаётся случайными значениям т.е. массив x(INPUT_DIM)
-
-w1 = np.random.randn(INPUT_DIM, H_DIM) # Матрица весов от одного слоя к другому
-b1 = np.random.randn(H_DIM) # это массив весов для нейрона смещения (при его наличии)
-w2 = np.random.randn(H_DIM, OUT_DIM) 
-b2 = np.random.randn(OUT_DIM)
 
 def relu(t):
     return np.maximum(t, 0) # Всё также используется numpy которая получая вектор, вернёт также вектор
@@ -28,6 +18,56 @@ def predict(x):
     z = softmax(t2)
     return z
 
-probs = predict(x)
-pred_class = np.argmax(probs) # Это наш ответ в виде индекса нейрона выхода
-print('choice:', pred_class)
+
+# -------
+# Модуль отвечающий за обучение. Должен принять вектор ошибок и выполнить обучение
+
+# -------
+# Модуль генерации ошибки. Должен вычислить ошибку согласно алгоритму
+class ErrorGenerator:
+    def __init__(self):
+        self.prev_predict = 0 # предсказание на предыдущем шаге
+
+    def calculate(self, vecctor, choise):
+        if (self.prev_predict == 0):
+            self.prev_predict = vecctor[choise]
+        else: 
+            error = vecctor[choise] - self.prev_predict # возможно наоборот
+            self.prev_predict = vecctor[choise]
+            return error
+
+
+if __name__ == '__main__':
+
+    INPUT_DIM = 4 # количество входных значений
+    OUT_DIM = 3 # количество вsходных значений
+    H_DIM = 5 # количество нейронов на слое (пока 1 слой)
+    err_gen = ErrorGenerator()
+
+    previous_step = 0
+
+    x = np.random.randn(INPUT_DIM) # Входящий вектор, пока задаётся случайными значениям т.е. массив x(INPUT_DIM)
+
+    w1 = np.random.randn(INPUT_DIM, H_DIM) # Матрица весов от одного слоя к другому
+    b1 = np.random.randn(H_DIM) # это массив весов для нейрона смещения (при его наличии)
+    w2 = np.random.randn(H_DIM, OUT_DIM) 
+    b2 = np.random.randn(OUT_DIM)
+
+for i in range(1, 6):
+    print("Итерация", i)
+    time.sleep(1)
+    #* Нейросеть считает ожидаемую награду за действия и выбирает необходимое. 
+    predicted_rewards = predict(x)
+    # На выходных нейронах мы будем получать рассчитаную награду за действия
+    choise = np.argmax(predicted_rewards) # Это наш ответ в виде индекса нейрона выхода
+    print('choice:', choise)
+
+    #! Получается что мы будем сначала сканировать всё окружение и передавать все значения сканера одним вектором.
+
+    #* Среда выполняет действие.
+    x = np.random.randn(INPUT_DIM)
+
+    #* Отправляем задачу в модуль вычисления ошибки. У нас есть текущий вектор решений и выбор. Их и будем передавать
+    error = err_gen.calculate(predicted_rewards, choise)
+
+    #* Запускаем обучение
