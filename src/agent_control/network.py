73f414c88a12f16d609bbc03f09ae24dfa13cc0f
Anton_arl <CelestialArlate@gmail.com>
2024-04-29 00:25:41 +0700

    temp_ error network line 94

--- src/agent_control/network.py
+++ src/agent_control/network.py
@@ -24,7 +24,7 @@ class Layer:
         self.next_layer = None
 
     def set_prev_layer(self, prev_layer):
-        self.prev_layer = prev_layer
+        self.prev_layer:Layer = prev_layer
         if prev_layer is not None:
             m = prev_layer.N
             n = self.N
@@ -32,11 +32,20 @@ class Layer:
             self.E_w = np.zeros((m, n))
 
     def set_next_layer(self, next_layer):
-        self.next_layer = next_layer
+        self.next_layer:Layer = next_layer
 
 
 def relu(t):
-    return np.maximum(t, 0) # Всё также используется numpy которая получая вектор, вернёт также вектор
+    a = -0.01
+    x = np.maximum(t, 0)
+    x[x == 0] = a * t[x == 0]
+    return x # Всё также используется numpy которая получая вектор, вернёт также вектор
+
+def deriv_relu(t):
+    a = -0.1
+    x = np.where(t > 0, 1, a)
+    return x
+
 
 def softmax(t):
     out = np.exp(t) 
@@ -67,18 +76,34 @@ def predict_full(network):
 # -------
 # Модуль отвечающий за обучение. Должен принять вектор ошибок и выполнить обучение
 
-def bacward(layer):
-    # Потребуется для каждого слоя:
-    # {i} буду отмечать как индекс слоя
-    # dE|dt{i} Выход ошибки на текущем слое
-    # dE|dw{i} = h{i-1}.T @ dE|dt{i} Важно понимать что индекс весов равен индексу следующего в прямом порядке слою
-    # dE|db{i} = dE|dt {i}
-    # dE|dh{i-1} = dE|dt{i} @ w{i}.T
-    # dE|dt{i-1} = dE|dh{i-1} * deriv(t{?}) пока непонял какой индекc у t;
-    # на этом мы получаем исходящую ошибку для предыдущего слоя
-    # далее запуск обучения выглядит следующим образом
-    # w{i} = w{i} - ALPHA * dE|dw{i} ; ALPHA - коофициент обучения
-    pass
+class Backward:
+    def one_lay(self, layer: Layer):
+        # Потребуется для каждого слоя:
+        # {i} буду отмечать как индекс слоя
+        # dE|dt{i} Выход ошибки на текущем слое
+        # dE|dw{i} = h{i-1}.T @ dE|dt{i} Важно понимать что индекс весов равен индексу следующего в прямом порядке слою
+        # dE|db{i} = dE|dt {i}
+        # dE|dh{i-1} = dE|dt{i} @ w{i}.T
+        # dE|dt{i-1} = dE|dh{i-1} * deriv(t{i-1}) ; t - вход слоя
+        # на этом мы получаем исходящую ошибку для предыдущего слоя
+        # далее запуск обучения выглядит следующим образом
+        # w{i} = w{i} - ALPHA * dE|dw{i} ; ALPHA - коофициент обучения
+
+        LEARNING_RATE = 0.001
+
+        layer.E_w = layer.prev_layer.Out.T @ layer.E_Out
+        layer.E_b = layer.E_Out
+        layer.prev_layer.E_In = layer.E_Out @ layer.w.T
+        layer.prev_layer.E_Out = layer.prev_layer.E_Out * deriv_relu(layer.prev_layer.In)
+
+        layer.w = layer.w - LEARNING_RATE * layer.E_w
+
+    def full_net(self, network: list[Layer]):
+        layer = network[len(network)-1]
+        while layer.prev_layer != None:
+            self.one_lay(layer)
+            layer = layer.prev_layer
+        pass
 
 # -------
 # Модуль генерации ошибки. Должен вычислить ошибку согласно алгоритму
@@ -134,6 +159,7 @@ if __name__ == '__main__':
     OUT_DIM = network[2].N # количество вsходных значений
     H_DIM = network[1].N # количество нейронов на слое (пока 1 слой)
     err_gen = ErrorGenerator()
+    backward = Backward()
 
     previous_step = 0
 
@@ -165,13 +191,17 @@ for i in range(1, 6):
     #! Получается что мы будем сначала сканировать всё окружение и передавать все значения сканера одним вектором.
 
     #* Среда выполняет действие.
-    x = np.random.randn(INPUT_DIM)
-    network[0].Out = np.random.randn(INPUT_DIM)
+    # x = np.random.randn(INPUT_DIM)
+    # network[0].Out = np.random.randn(INPUT_DIM)
+    # для теста вход одинаковый
 
     #* Отправляем задачу в модуль вычисления ошибки. У нас есть текущий вектор решений и выбор. Их и будем передавать
     # error = err_gen.calculate(predicted_rewards, choise)
     dE_dt2 = err_gen.calculate(predicted_rewards, choise) # получили вектор выхода ошибок для слоя OUT
     
+    network[len(network)-1].E_Out = dE_dt2
+    
     print('errors:', dE_dt2)
 
     #* Запускаем обучение
+    backward.full_net(network)
