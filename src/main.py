642396e885789ba11d64b13adb9a4318573a0ca9
Anton_arl <CelestialArlate@gmail.com>
2024-05-31 14:51:52 +0700

    work progress

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ import labyrinth.Contact as labyrinth
 
 root = labyrinth.tk  # Получаем корневой виджет Tkinter
 
-def draw_target(canvas, x, y, size=5, color='green'):
+def drawTarget(canvas, x, y, size=5, color='green'):
     """
     Функция для отрисовки значка цели на карте.
     
@@ -53,40 +53,81 @@ def trascriptAnswer(vecctor):
     key_symbol = key_map.get(choise, "")
     return choise, key_symbol
 
+def calculateReward(distance):
+    if not hasattr(calculateReward, "prev_distance"):
+        calculateReward.prev_distance = 0
+    reward = 0
+    if calculateReward.prev_distance == distance:
+        reward = reward - 0.001
+    else:
+        reward = 1 / (abs(distance))
+    calculateReward.prev_distance = distance
+    return reward
+
 def second_cycle():
     global count
-    # В начале нам надо собрать данные
     global target_coordinates
+    gamma = 0.9
+
+    state = network[0].Out
+    # вызываем решение неейросети с прошлой итерации (позже надо будет метод написать)
+    prev_predicted_rewards = network[len(network)-1].Out
+    # обращаемся к дополнительному модулю для трансфера ответа нейросети в нажатие кннопки.
+    action, key_symbol = trascriptAnswer(prev_predicted_rewards) # в action нужно решение для текущих данных (можно получить с прошлой итерации)
+    # Среда выполняет действие
+    labyrinth.canvas.event_generate(f"<KeyPress-{key_symbol}>")
+    # Надо собрать данные, новое состояние и награду
+
+    # В начале нам надо собрать данные
     current_coordinates = labyrinth.obj.getCoords()
 
     x_diff = target_coordinates[0] - current_coordinates[0]
     y_diff = target_coordinates[1] - current_coordinates[1]
     distance = math.sqrt(x_diff**2 + y_diff**2)
-    reward = 1 / (abs(distance) + 1)
-    
 
-    # передаём в нейросеть данные
-    incoming_data = np.hstack((np.array(target_coordinates), np.array(current_coordinates))).reshape(1, -1)
-    incoming_data = nl.normalize_data(incoming_data)
-    network[0].Out = incoming_data
-    # передаём данные на решение неейросети
+    # конвертируем данные в вектор нейросети
+    next_state = np.hstack((np.array(target_coordinates), np.array(current_coordinates))).reshape(1, -1)
+    next_state = nl.normalize_data(next_state)
+
+    # Запускаем вычислитель награды
+    reward = calculateReward(distance)
+
+    # Вычисляем предсказание награды на следующем шагу
+    network[0].Out = next_state
     predicted_rewards = forward(network)
-    # обращаемся к дополнительному модулю для трансфера ответа нейросети в нажатие кннопки.
-    choise, key_symbol = trascriptAnswer(predicted_rewards)
-    # Отправляем задачу в модуль вычисления ошибки. 
-    network[len(network)-1].E_Out = err_gen.calculate(predicted_rewards, choise, reward) 
+    td_target = reward + gamma * np.max(predicted_rewards) # Нужен вектор решений для новых данных
+    # т.е. на прошлом шагу должно было получиться это
+    # но нейронка предсказала это prev_predicted_rewards
+    # td_error = td_target - np.max(prev_predicted_rewards)
+    td_error = np.max(prev_predicted_rewards) - td_target
+
+    # принудительно переписываем значения чтобы понять как работает
+    # td_error = 0.1 # ошибка
+    # action = 0 # на индексе
+
+    print("reward: ", reward)
+    print("PRreward: ", prev_predicted_rewards)
+    print("target: ", td_target)
+    print("error: ", td_error)
+    print("choise: ", action)
+    print(network[len(network)-1].Out)
+    # Создаём и передаём нейросети вектор ошибок
+    network[len(network)-1].E_Out = np.zeros_like(network[len(network)-1].E_Out)
+    network[len(network)-1].E_Out += 0.0001
+    network[len(network)-1].E_Out[0][action] = td_error
+    # Запускаем обучение
+    backward.full_net(network)
+
     
     if count > 100:
-        print(choise)
+        print(action)
         print(reward)
-        print("ERROR", network[len(network)-1].E_Out[0][choise])
+        print("ERROR", network[len(network)-1].E_Out[0][action])
         count = 0
     count = count + 1
 
-    # Запускаем обучение
-    backward.full_net(network)
-    # Среда выполняет действие
-    labyrinth.canvas.event_generate(f"<KeyPress-{key_symbol}>")
+
+
 
     if target_coordinates == current_coordinates: 
         print('!!!DONE!!!')
@@ -95,10 +136,12 @@ def second_cycle():
 
     # labyrinth.canvas.event_generate("<KeyPress-Left>")
     # print('object X:', labyrinth.obj.x)
-    root.after(5, second_cycle)  # Интервал работы нейросети
+    root.after(50, second_cycle)  # Интервал работы нейросети
 
 
-draw_target(labyrinth.canvas, target_coordinates[0], target_coordinates[1])
+drawTarget(labyrinth.canvas, target_coordinates[0], target_coordinates[1])
+network[0].Out = network[0].Out + 0.00001
+
 root.after(0, second_cycle)  # Запускаем Вторичный цикл для работы нейросети
 
 
